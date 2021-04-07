7e7c64083baaabf4d3de642e5be35502c016dd1e
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-04-07 13:45:02 -0400

    lecture work-in-progress

--- followAlong/src/components/SignupForm.js
+++ followAlong/src/components/SignupForm.js
@@ -21,23 +21,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const useForm = (initialValues) => {
-  const [values, setValue] = useState(initialValues);
 
-  const handleChanges = e => {
-    setValue({
-      ...values,
-      [e.target.name]: e.target.value
-    });
-  };
-
-  const clearForm = e => {
-    e.preventDefault();
-    setValue(initialValues);
-  };
-
-  return([ values, handleChanges, clearForm ]);
-}
 
 const initialValues = {
   firstName:"", 
---
+++ followAlong/src/hooks/useForm.js
@@ -0,0 +1,21 @@
+import {useState} from 'react';
+
+const useForm = (initialValues) => {
+    const [values, setValue] = useState(initialValues);
+  
+    const handleChanges = e => {
+      setValue({
+        ...values,
+        [e.target.name]: e.target.value
+      });
+    };
+  
+    const clearForm = e => {
+      e.preventDefault();
+      setValue(initialValues);
+    };
+  
+    return([ values, handleChanges, clearForm ]);
+}
+
+export default useForm;
