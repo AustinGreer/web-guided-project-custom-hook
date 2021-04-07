d35befa2d2b0f97c8c486ce56965177d9b736e14
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-04-07 13:40:22 -0400

    lecture work-in-progress

--- followAlong/src/components/SignupForm.js
+++ followAlong/src/components/SignupForm.js
@@ -25,10 +25,8 @@ const useForm = (initialValues) => {
   const [values, setValue] = useState(initialValues);
 
   const handleChanges = e => {
-    console.log(e.target.name);
-    
     setValue({
-      lastName: e.target.value
+      [e.target.name]: e.target.value
     });
   };
 
